a4e03f8b09d6906b813dc3c9941c9ca1e71e3575
Zeimd <jv.korkala@gmail.com>
2023-01-05 08:23:42 +0200

    Cleanup. Add comments.

--- casm/linker.cpp
+++ casm/linker.cpp
@@ -125,7 +125,10 @@ Ceng::CRESULT Linker::LinkProgram(std::vector<Casm::ObjectCode*> &objects,
 	std::vector<ProgramSection> progSections;
 
 	// Calculate offsets that sections will have after
-	// merging
+	// merging.
+	//
+	// NOTE: Don't copy sections yet so that following
+	//       relocation step has a bit easier math
 
 	Ceng::UINT64 offset = 0;
 
@@ -147,7 +150,6 @@ Ceng::CRESULT Linker::LinkProgram(std::vector<Casm::ObjectCode*> &objects,
 
 	std::vector<Casm::RelocationData> relocationData;
 
-	
 	for (auto& file : objects)
 	{
 		for (auto& relocation : file->relocationData)
@@ -194,11 +196,7 @@ Ceng::CRESULT Linker::LinkProgram(std::vector<Casm::ObjectCode*> &objects,
 				break;
 			case RelocationType::full_int32:
 
-				if (symbol == nullptr)
-				{
-
-				}
-				else
+				if (symbol != nullptr)
 				{
 					Ceng::INT32* ptr =
 						(Ceng::INT32*)&relocationSection->codeBuffer[relocation.writeOffset];
@@ -229,11 +227,11 @@ Ceng::CRESULT Linker::LinkProgram(std::vector<Casm::ObjectCode*> &objects,
 			
 		}			
 	}
-	
 
-	size_t k;
+	// Copy sections to new buffer according to offsets
+	// calculated earlier
 
-	for (k=0; k < sectionInfo.size();++k)
+	for (size_t k=0; k < sectionInfo.size();++k)
 	{
 		for (auto& file : objects)
 		{
@@ -248,138 +246,6 @@ Ceng::CRESULT Linker::LinkProgram(std::vector<Casm::ObjectCode*> &objects,
 	}
 
 	output = std::make_shared<Program>(std::move(relocationData), std::move(progSections));
-
-
-
-
-	// Find entry point function
-
-	/*
-
-	std::shared_ptr<X86::ObjectFunction> entryPoint = nullptr;
-
-	Ceng::UINT32 k,j;
-
-	for(k=0;k<objects.size();k++)
-	{
-		for(j=0;j<objects[k]->functions->size();j++)
-		{
-			std::vector<std::shared_ptr<X86::ObjectFunction>> *temp = objects[k]->functions;
-
-			if ( (*temp)[k]->name == entryFunction)
-			{
-				entryPoint = (*temp)[k];
-			}
-		}
-	}
-
-	entryPoint->IncRefCount();
-
-	std::vector<std::shared_ptr<X86::SymbolRef>> *linkDisplacements;
-	std::vector<std::shared_ptr<X86::Symbol>> *linkData;
-		
-	std::vector<std::shared_ptr<X86::Symbol>> *linkFunctions;
-
-	linkData = new std::vector<std::shared_ptr<X86::Symbol>>();
-	linkFunctions = new std::vector<std::shared_ptr<X86::Symbol>>();
-	linkDisplacements = new std::vector<std::shared_ptr<X86::SymbolRef>>();
-
-	// Gather symbols that will be present in the final executable
-
-	linkFunctions->push_back(entryPoint);
-
-	k = 0;
-	do
-	{
-		// NOTE: ReferenceAll adds all functions called to linkFunctions
-		//       if they are currently unreferenced
-
-		X86::ObjectFunction *objFunc = (*linkFunctions)[k]->AsObjectFunction();
-
-		objFunc->ReferenceAll(linkData,linkFunctions);
-
-		k++;
-	} while (k < linkFunctions->size());
-
-	// Allocate variables to data segment
-
-	Ceng::UINT32 dataSegmentSize = 0;
-
-	for(k=0;k<linkData->size();k++)
-	{
-		(*linkData)[k]->SetOffset(dataSegmentSize);
-		
-		X86::DataItem *item = (*linkData)[k]->AsData();
-
-		dataSegmentSize += item->Size();
-	}
-
-	// Build code section
-
-	Ceng::UINT32 codeSegSize = 0;
-	
-	for(k=0;k<linkFunctions->size();k++)
-	{
-		// TODO: Pad function offset to start at cache line if necessary
-
-		(*linkFunctions)[k]->SetOffset(codeSegSize);
-
-		X86::ObjectFunction *objFunc = (*linkFunctions)[k]->AsObjectFunction();
-
-		if (objFunc->SizeBytes() > 0)
-		{
-			codeSegSize += objFunc->SizeBytes();
-		}
-	}
-
-	Ceng::AlignedBuffer<Ceng::UINT8> dataSegment; 
-	
-	dataSegment = Ceng::AlignedBuffer<Ceng::UINT8>::AlignedBuffer(dataSegmentSize,cacheLine);
-
-	// Write initial values of data items
-
-	for(k=0;k< linkData->size();k++)
-	{
-		X86::DataItem *item = (*linkData)[k]->AsData(); 
-
-		item->initializer->WriteValues(&dataSegment[(Ceng::UINT32)(*linkData)[k]->Offset()]);
-	}
-
-	// Build linked program
-
-	X86::Program *program = new X86::Program();
-
-	Ceng::AlignedBuffer<Ceng::UINT8> codeBuffer;
-
-	codeBuffer = Ceng::AlignedBuffer<Ceng::UINT8>::AlignedBuffer(codeSegSize,cacheLine);
-
-	memset(&codeBuffer[0], 0, codeBuffer.GetSize());
-
-	program->entryPoint = 0;
-
-	for(k=0;k<linkFunctions->size();k++)
-	{
-		X86::ObjectFunction *objFunc = (*linkFunctions)[k]->AsObjectFunction();
-
-		if (objFunc->SizeBytes() == 0) continue;
-		
-		objFunc->WriteAllOffsets();
-
-		uint32_t offset = uint32_t(objFunc->Offset());
-
-		objFunc->ToCodeBuffer(&codeBuffer[offset]);
-		objFunc->AppendRelocationData(program->relocationData);
-	}
-
-	program->codeBuffer = std::move(codeBuffer);
-	program->dataSection = std::move(dataSegment);
-
-	delete linkData;
-	delete linkDisplacements;
-	delete linkFunctions;
-
-	*output = program;
-	*/
 
 	return Ceng::CE_OK;
 }
